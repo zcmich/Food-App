8bed0feaf270d03b06e83c1f9599551cb88f8440
zcmich <zafackmichel@yahoo.fr>
2023-08-08 11:14:28 +0100

    cart contex added

--- src/Components/Layout/HeadCartButton.js
+++ src/Components/Layout/HeadCartButton.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import CartIcon from '../Cart/CartIcon';
+import {useContext} from 'react';
 
+import CartIcon from '../Cart/CartIcon';
 import classes from './HeadCartButton.module.css'
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+    const cardCtx = useContext(CartContext);
+
+    const numberOfCartItems = cardCtx.items.reduce((curNumber, item) =>{
+        return curNumber + item.amount;
+    }, 0)
+
     return (
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>YOUR CART</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
 
     )
