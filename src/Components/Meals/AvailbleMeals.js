259a04e6a590a559ab9ed5cda3167a25c8df4c94
zcmich <zafackmichel@yahoo.fr>
2023-10-18 17:06:44 +0100

    fetch available meals http

--- src/Components/Meals/AvailbleMeals.js
+++ src/Components/Meals/AvailbleMeals.js
@@ -1,50 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import classes from './AvailableMeals.module.css'
 import Card from "../UI/Card";
 import MealItem from "./Mealtem/MealItem";
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
-
 const AvailableMeals = (props) => {
-    const mealsList = DUMMY_MEALS.map(meal =>
-        <MealItem
-            id={meal.id}
-            key={meal.id}
-            name={meal.name}
-            description={meal.description}
-            price={meal.price}
-        />);
+    const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+
+            setIsLoading(true);
+            const response = await fetch('https://react-http1-ea92c-default-rtdb.europe-west1.firebasedatabase.app//meals.json');
+            if (!response.ok) {
+                throw new Error("Cliff there was an error fetching availble meals")
+            }
+            const data = await response.json();
+            console.log(data);
+
+            const availableMeals = [];
+
+            for (const key in data) {
+                availableMeals.push({
+                    id: key,
+                    name: data[key].name,
+                    description: data[key].description,
+                    price: data[key].price
+                })
+            }
+            setMeals(availableMeals);
+            setIsLoading(false);
+        };
+
+        fetchMeals().catch(error => {
+            setIsLoading(false);
+            setError(error.message);
+        });
+
+    }, [])
+
+    let content = <p>Found no available meals.</p>;
+
+    if (isLoading) {
+        return <section className={classes.spinner}></section>;
+    }
+
+    if (meals.length > 0) {
+        content = meals.map(meal =>
+            <MealItem
+                id={meal.id}
+                key={meal.id}
+                name={meal.name}
+                description={meal.description}
+                price={meal.price}
+            />);
+    }
+
+    if (error) {
+        content = <h2 className={classes.MealsError}>{error}</h2>
+    }
+
     return (
         <section className={classes.meals}>
             <Card>
                 <ul>
-                    {mealsList}
+                    {content}
                 </ul>
             </Card>
         </section>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -13,7 +13,7 @@ const cartReducer = (state, action) => {
 
         const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
         const existingCartItem = state.items[existingCartItemIndex];
-        
+
         let updatedItems;
 
         if (existingCartItem) {
@@ -25,7 +25,7 @@ const cartReducer = (state, action) => {
             updatedItems = [...state.items]
             updatedItems[existingCartItemIndex] = updatedItem;
         } else {
-           updatedItems = state.items.concat(action.item);
+            updatedItems = state.items.concat(action.item);
         }
 
         return {
@@ -33,6 +33,33 @@ const cartReducer = (state, action) => {
             totalAmount: updatedTotalAmount
         };
     }
+
+    if (action.type === 'REMOVE') {
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+        let updatedItems;
+
+        if (existingCartItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+
+        } else {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount - 1
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+
+    }
+
     return defaultCartState;
 };
 
